8d493023a09f279cbbeebba56aecc1138664d8f4
Heena <heena.madan4@gmail.com>
2020-04-02 13:33:52 +0530

    read me changes

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome Heena</h1>
         </header>
         <p className="App-intro">
-          I was changed on the Feature branch
+          This is my first Beanstalk docker app on React of using Travis
         </p>
       </div>
     );
